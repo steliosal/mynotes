d3fe6dd3c972a8a3d1400159d781ccbc00238db6
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-10-27 16:45:48 +0300

    ready to deploy first version

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {ReactComponent as AddIcon} from '../assets/plusIcon.svg'
+import { ReactComponent as AddIcon } from "../assets/addIcon.svg";
 
 const AddButton = () => {
     return (
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ListItem = ({ note }) => {
-  return (
-    <div className="notes-list-item">
-      <Link to={`/note/${note.id}`}>
-        <h3>{note.body}</h3>
-      </Link>
-    </div>
-  );
+
+const getTitle = (note) => {
+  const title = note.body.split("\n")[0];
+  if (title.length > 45) {
+    return title.slice(0, 45) + "...";
+  }
+  return title;
+};
+
+const getTime = (note) => {
+  const time = new Date(note.updated).toLocaleDateString();
+  return time;
+};
+
+return (
+  <div className="notes-list-item">
+    <Link to={`/note/${note.id}`}>
+      <h3>{getTitle(note)}</h3>
+      <p>
+        <span>{getTime(note)}</span>
+      </p>
+    </Link>
+  </div>
+);
 };
 
 export default ListItem;
